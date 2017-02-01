f27b867ec95b34eb55169d3edb0b8873c581870c
bsokat <barissokat@gmail.com>
2017-02-01 12:07:12 +0300

    Added Model classes and DataContext

---
+++ NewsPortal.Data/DataContext/NewsContext.cs
@@ -0,0 +1,22 @@
+﻿using NewsPortal.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Data.DataContext
+{
+    public class NewsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ NewsPortal.Data/Model/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Data.Model
+{
+    public class Role
+    {
+        public int ID { get; set; }
+        [Display(Name = "Rol Adı:")]
+        public string RoleName { get; set; }
+    }
+}
