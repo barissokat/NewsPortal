493f81de3145624d37443792de2b42c8b9a550ce
bsokat <barissokat@gmail.com>
2017-02-07 00:00:56 +0300

    Updated Admin and Data

---
+++ NewsPortal.Admin/Classes/BootStrapper.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsPortal.Admin.Classes
+{
+    public class BootStrapper
+    {
+        //Boot aşamasında çalışacak
+        public static void RunConfig()
+        {
+            BuildAutoFac();
+        }
+        private static void BuildAutoFac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //Interface ler yazılacak core içindeki
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
