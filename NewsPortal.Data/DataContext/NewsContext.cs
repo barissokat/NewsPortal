cdc732a99c0eb351ba739124beeadf92ae9f5a27
bsokat <barissokat@gmail.com>
2017-02-07 16:45:51 +0300

    Added Interfaces

--- NewsPortal.Data/DataContext/NewsContext.cs
+++ NewsPortal.Data/DataContext/NewsContext.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace NewsPortal.Data.DataContext
 {
-    class NewsContext : DbContext
+    public class NewsContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
-        public DbSet<News> New_s { get; set; }
+        public DbSet<News> Newss { get; set; }
         public DbSet<Image> Images { get; set; }
     }
 }
