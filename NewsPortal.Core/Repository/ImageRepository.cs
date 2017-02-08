d8a85cf484996e72cc940eb6fa55c575a5b7474c
bsokat <barissokat@gmail.com>
2017-02-08 19:59:51 +0300

    Updated Repository

--- NewsPortal.Core/Repository/ImageRepository.cs
+++ NewsPortal.Core/Repository/ImageRepository.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using NewsPortal.Data.Model;
 using System.Linq.Expressions;
 using NewsPortal.Data.DataContext;
+using System.Data.Entity.Migrations;
 
 namespace NewsPortal.Core.Repository
 {
