85f78e6a76fdaf0992bfe4b49129d378ea5a7b58
bsokat <barissokat@gmail.com>
2017-02-12 12:43:19 +0300

    Category Repository added

---
+++ NewsPortal.Core/Infrastructure/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using NewsPortal.Core.Infrastructure;
+using NewsPortal.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Core.Infrastructure
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+    }
+}
---
+++ NewsPortal.Core/Repository/CategoryRepository.cs
@@ -0,0 +1,66 @@
+﻿using NewsPortal.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Data.Model;
+using System.Linq.Expressions;
+using NewsPortal.Data.DataContext;
+using System.Data.Entity.Migrations;
+
+namespace NewsPortal.Core.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly NewsContext _context = new NewsContext();
+        public int Count()
+        {
+            return _context.Categories.Count();
+        }
+
+        public void Delete(int id)
+        {
+            var category = GetById(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+        }
+
+        public Category Get(Expression<Func<Category, bool>> expression)
+        {
+            return _context.Categories.FirstOrDefault(expression);
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            return _context.Categories.Select(x => x);
+        }
+
+        public Category GetById(int id)
+        {
+            return _context.Categories.FirstOrDefault(x => x.ID == id);
+        }
+
+        public IQueryable<Category> GetMany(Expression<Func<Category, bool>> expression)
+        {
+            return _context.Categories.Where(expression);
+        }
+
+        public void Insert(Category obj)
+        {
+            _context.Categories.Add(obj);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _context.Categories.AddOrUpdate(obj);
+        }
+    }
+}
