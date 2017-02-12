4b0d8c1e23f1ad7b824f73e7f8b3886671533298
bsokat <barissokat@gmail.com>
2017-02-12 12:42:34 +0300

    Category Controller and Views added

---
+++ NewsPortal.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using NewsPortal.Core.Infrastructure;
+using NewsPortal.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsPortal.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        #region Category
+        private readonly ICategoryRepository _categoryRepository;
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        #endregion
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Create(Category category)
+        {
+            return Json(1, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
