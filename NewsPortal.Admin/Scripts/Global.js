9003fce839af4d4018db89bc581d8d48c98601fd
bsokat <barissokat@gmail.com>
2017-02-12 13:38:45 +0300

    Script added for Create Category

---
+++ NewsPortal.Admin/Scripts/Global.js
@@ -0,0 +1,21 @@
+﻿function CreateCategory() {
+    Category = new Object();
+    Category.Name = $("#categoryName").val();
+    Category.Url = $("#categoryUrl").val();
+    Category.Active = $("#categoryActive").is(":checked");
+
+    $.ajax({
+        url: "Category/Create",
+        data: Category,
+        type: "POST",
+        success: function (response) {
+            if (response.success) {
+                alert("Başarılı işlemlerini yap.");
+            }
+            else {
+                alert("Başarısız işlemlerini yap.")
+            }
+        }
+    });
+    
+}
