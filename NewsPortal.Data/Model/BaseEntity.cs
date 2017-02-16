793f1f5e51b2cf34f19d4ecb7eee53175889a52b
bsokat <barissokat@gmail.com>
2017-02-16 09:20:23 +0300

    Added inherit class

---
+++ NewsPortal.Data/Model/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Data.Model
+{
+    public class BaseEntity
+    {
+        private DateTime date = DateTime.Now;
+        private bool active = true;
+
+        public int ID { get; set; }
+
+        [Display(Name = "Yüklenme Tarihi:")]
+        public DateTime UploadDate { get { return date; } set { date = value; } }
+
+        [Display(Name = "Aktiflik:")]
+        public bool Active { get { return active; } set { active = value; } }
+    }
+}
